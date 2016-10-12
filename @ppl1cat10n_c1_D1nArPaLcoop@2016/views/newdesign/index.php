45d1541146510345d75f3a4f159603539d5e295b
ika <zulaikhaabdullah75@gmail.com>
2016-10-12 12:48:50 +0800

    ??

--- @ppl1cat10n_c1_D1nArPaLcoop@2016/views/newdesign/index.php
+++ @ppl1cat10n_c1_D1nArPaLcoop@2016/views/newdesign/index.php
@@ -5,7 +5,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <meta name="description" content="">
         <meta name="author" content="">
-        <title>DinarPal</title>
+        <title>Kodinar</title>
         <link rel="shortcut icon" type="image/x-icon" href="<?php echo base_url() . 'assets/design_ori/favicon.ico' ?>">
         <!-- core CSS -->
         <link href="<?php echo base_url(); ?>assets/csss/bootstrap.min.css" rel="stylesheet">
